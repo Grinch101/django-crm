6d26ca13d95af3806a4636248b5fb51d57f3b3bd
Grinch101 <Masoud.Kameli@gmail.com>
2021-05-11 00:37:47 +0430

    minor changes in utility helpers

--- CRM/CRM/utility.py
+++ CRM/CRM/utility.py
@@ -3,12 +3,14 @@ from functools import wraps
 from CRM.settings import SECRET_KEY
 from apps.user.models import Users
 from jwt import DecodeError
+from django.core import serializers
 import jwt
 
+
 def json_output(data='', error='', info='', status=200):
-    output_dic ={'data':data,
-                 'error':error,
-                 'info':info}
+    output_dic = {'data': data,
+                  'error': error,
+                  'info': info}
     return JsonResponse(data=output_dic, safe=True, status=status)
 
 
@@ -18,17 +20,21 @@ def login_required(func):
         token = request.headers['JWT']
         if token:
             try:
-                payload = jwt.decode(token, key=SECRET_KEY, algorithms=["HS256"])
+                payload = jwt.decode(token, key=SECRET_KEY,
+                                     algorithms=["HS256"])
                 user_id = int(payload['user_id'])
                 rows = Users.objects.filter(id=user_id)
                 if len(rows) > 0:
-                    return func(request, user_id=user_id, *args, **kwargs)
+                    return func(request, user_id=rows[0].id, *args, **kwargs)
                 else:
-                    return json_output(error='User not found!', status= 404) # not found!
+                    # not found!
+                    return json_output(error='User not found!', status=404)
             except DecodeError as e:
-                return json_output(error=f'Token invalid,  {e}', status= 403) # forbidden!
+                # forbidden!
+                return json_output(error=f'Token invalid,  {e}', status=403)
         else:
-            return json_output(error= 'Please login!', status= 401) # unauthorized!
+            # unauthorized!
+            return json_output(error='Please login!', status=401)
     return wrap
 
 
@@ -36,6 +42,6 @@ def pass_pop(row):
     user_id = row.id
     email = row.email
     client_name = row.client_name
-    return {'user_id':user_id,
-            'email':email,
-            'client_name':client_name}
+    return {'user_id': user_id,
+            'email': email,
+            'client_name': client_name}
